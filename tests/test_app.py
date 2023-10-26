e38cae9e0709b7e94ab58a209aaf4bc8f4f38416
leozw <lzwdeveloper@gmail.com>
2023-10-26 20:03:06 -0300

    test ArgoCD with Actions

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,20 +2,15 @@ import unittest
 from unittest.mock import patch
 from flask import Flask
 from app import app as flask_app
-from opentelemetry import trace
-from opentelemetry.sdk.resources import Resource
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
-from opentelemetry.instrumentation.flask import FlaskInstrumentor
 
 class TestRollDice(unittest.TestCase):
     def setUp(self):
-        self.app = flask_app  # Use a app importada
+        self.app = flask_app
+        self.app.testing = True  # Configura o Flask em modo de teste
         self.client = self.app.test_client()
 
     def test_roll_dice(self):
-        with patch('random.randint', return_value=3):
+        with patch('app.roll', return_value='3'):  # Patch the roll function in app
             response = self.client.get('/rolldice')
             self.assertEqual(response.status_code, 200)
             self.assertEqual(response.data.decode(), '3')
