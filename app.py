61201b54446be5d05bd660195d33edc82a490189
Leonardo Gabriel Zwirtes <53581041+leozw@users.noreply.github.com>
2023-10-26 18:30:35 -0300

    test sonar

--- app.py
+++ app.py
@@ -7,33 +7,37 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 
-# Configuration OpenTelemetry
-trace.set_tracer_provider(
-    TracerProvider(
-        resource=Resource.create(attributes={"service.name": "rolldice-service"})
+def configure_opentelemetry():
+    trace.set_tracer_provider(
+        TracerProvider(
+            resource=Resource.create(attributes={"service.name": "rolldice-service"})
+        )
     )
-)
+    
+    # Docker-compose
+    # otlp_exporter = OTLPSpanExporter(endpoint="otel-collector:4317", insecure=True)
 
-# Docker-compose
-# otlp_exporter = OTLPSpanExporter(endpoint="otel-collector:4317", insecure=True)
+    # Kuberntes
+    otlp_exporter = OTLPSpanExporter(endpoint="opentelemetrycollector.elastic-stack.svc.cluster.local:4317", insecure=True)
 
-# Kuberntes
-otlp_exporter = OTLPSpanExporter(endpoint="opentelemetrycollector.elastic-stack.svc.cluster.local:4317", insecure=True)
+    span_processor = BatchSpanProcessor(otlp_exporter)
 
-span_processor = BatchSpanProcessor(otlp_exporter)
+    trace.get_tracer_provider().add_span_processor(span_processor)
 
-trace.get_tracer_provider().add_span_processor(span_processor)
-
-app = Flask(__name__)
-
-FlaskInstrumentor().instrument_app(app)
-
-@app.route("/rolldice")
 def roll_dice():
     return str(roll())
 
 def roll():
     return randint(1, 6)
 
+app = Flask(__name__)
+
+FlaskInstrumentor().instrument_app(app)
+
+@app.route("/rolldice")
+def rolldice_route():
+    return roll_dice()
+
 if __name__ == '__main__':
+    configure_opentelemetry()
     app.run(host='0.0.0.0')
